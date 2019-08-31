14f7faf41272372126f27e75c73532f4d5bd43ab
yukihane <yukihane.feather@gmail.com>
2019-08-31 12:45:25 +0900

    https://atcoder.jp/contests/practice/tasks/practice_1

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut iterator = stdin.lock().lines();
+    let line1 = iterator.next().unwrap().unwrap();
+    let line2 = iterator.next().unwrap().unwrap();
+    let line3 = iterator.next().unwrap().unwrap();
+
+    let a = line1.parse::<i32>().unwrap();
+
+    let mut w2 = line2.split_whitespace();
+    let b = w2.next().unwrap().parse::<i32>().unwrap();
+    let c = w2.next().unwrap().parse::<i32>().unwrap();
+
+    let text = line3;
+
+    println!("{} {}", a + b + c, text);
 }
