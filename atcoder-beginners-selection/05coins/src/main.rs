4149ee81ee079fa7cb81c6e55e416f35d103739f
yukihane <yukihane.feather@gmail.com>
2019-09-01 16:38:24 +0900

    debug logging除去

--- atcoder-beginners-selection/05coins/src/main.rs
+++ atcoder-beginners-selection/05coins/src/main.rs
@@ -9,18 +9,12 @@ fn main() {
     let c50 = ite.next().unwrap().unwrap().parse::<i32>().unwrap();
     let expected = ite.next().unwrap().unwrap().parse::<i32>().unwrap();
 
-    println!(
-        "500:{}, 100:{},50:{}, expected:{}",
-        c500, c100, c50, expected
-    );
-
     let mut matches = 0;
 
     let max500 = raise(expected, 500, c500);
     for n500 in (0..=max500).rev() {
         let total = totalize(n500, 0, 0);
         let shortage = expected - total;
-        println!("{}, total:{}, shortage:{}", n500, total, shortage);
         if shortage == 0 {
             matches += 1;
             continue;
@@ -30,7 +24,6 @@ fn main() {
         for n100 in (0..=max100).rev() {
             let total = totalize(n500, n100, 0);
             let shortage = expected - total;
-            println!("{}, {}, total:{}, shortage:{}", n500, n100, total, shortage);
             if shortage == 0 {
                 matches += 1;
                 continue;
@@ -40,10 +33,6 @@ fn main() {
             for n50 in (0..=max50).rev() {
                 let total = totalize(n500, n100, n50);
                 let shortage = expected - total;
-                println!(
-                    "{}, {}, {}, total:{}, shortage:{}, max50:{}",
-                    n500, n100, n50, total, shortage, max50
-                );
                 if shortage == 0 {
                     matches += 1;
                     continue;
