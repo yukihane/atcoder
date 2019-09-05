ac0df7aa759525e1199476c527c9b003b08d3662
yukihane <yukihane.feather@gmail.com>
2019-09-05 23:05:04 +0900

    アルゴリズム実装

--- atcoder-beginners-selection/10daydream/src/main.rs
+++ atcoder-beginners-selection/10daydream/src/main.rs
@@ -1,33 +1,66 @@
 fn main() {
     let text = "Hello, world!".chars().collect::<Vec<char>>();
     println!("{:?}", text);
-    calc("Hello, world!");
+    calc("erasedream");
 }
 
 fn calc(text: &str) -> bool {
     let mut input = text;
     loop {
+        // println!("{}", input);
         if input.len() == 0 {
             return true;
         } else if input.len() < 5 {
             // 今回の単語は最小でも文字数5
             return false;
         } else if input.starts_with("dream") {
-            match pop_dream(text) {
+            match pop_dream(input) {
                 Ok(res) => input = res,
                 Err(_) => return false,
             }
         } else if input.starts_with("erase") {
-            // TODO
+            match pop_erase(input) {
+                Ok(res) => input = res,
+                Err(_) => return false,
+            }
         } else {
             return false;
         }
     }
 }
 
-fn pop_dream(text: &str) -> Result<&str, String> {
-    // TODO
-    Ok(text)
+fn pop_dream(text: &str) -> Result<&str, &'static str> {
+    // println!("pop_dream");
+    if text == "dream" || text == "dreamer" {
+        // println!("OK!");
+        return Ok("");
+    }
+    let skipped = &text["dream".len()..];
+    if skipped.starts_with("d") || skipped.starts_with("era") {
+        // println!("x: {}", skipped);
+        return Ok(skipped);
+    }
+    let skipped = &text["dreamer".len()..];
+    if skipped.starts_with("d") || skipped.starts_with("e") {
+        // println!("y: {}", skipped);
+        return Ok(skipped);
+    }
+    Err("not match")
+}
+
+fn pop_erase(text: &str) -> Result<&str, &'static str> {
+    if text == "erase" || text == "eraser" {
+        return Ok("");
+    }
+    let skipped = &text["erase".len()..];
+    if skipped.starts_with("d") || skipped.starts_with("e") {
+        return Ok(skipped);
+    }
+    let skipped = &text["eraser".len()..];
+    if skipped.starts_with("d") || skipped.starts_with("e") {
+        return Ok(skipped);
+    }
+    Err("not match")
 }
 
 #[cfg(test)]
@@ -36,6 +69,14 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(calc("erasedream"), "YES");
+        assert_eq!(calc("erasedream"), true);
+    }
+    #[test]
+    fn test2() {
+        assert_eq!(calc("dreameraser"), true);
+    }
+    #[test]
+    fn test3() {
+        assert_eq!(calc("dreamerer"), false);
     }
 }
