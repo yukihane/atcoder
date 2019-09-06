aedaa1f92a3c7ebfdbf84c7ba88adbb7dbe70198
yukihane <yukihane.feather@gmail.com>
2019-09-06 20:57:04 +0900

    2点間の距離計測

--- atcoder-beginners-selection/11traveling/src/main.rs
+++ atcoder-beginners-selection/11traveling/src/main.rs
@@ -13,13 +13,51 @@ fn calc(records: &Vec<Record>) -> bool {
 }
 
 fn can_checkin(current: &Record, next: &Record) -> bool {
+    let distance = measure(current, next);
     false
+}
+
+fn measure(p1: &Record, p2: &Record) -> i32 {
+    let x = (p1.x - p2.x).abs();
+    let y = (p1.y - p2.y).abs();
+    x + y
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    #[test]
+    fn test_measure1() {
+        let distance = measure(
+            &Record {
+                time: 0,
+                x: 0,
+                y: 0,
+            },
+            &Record {
+                time: 3,
+                x: 1,
+                y: 2,
+            },
+        );
+        assert_eq!(distance, 3);
+    }
+    #[test]
+    fn test_measure2() {
+        let distance = measure(
+            &Record {
+                time: 0,
+                x: 10,
+                y: 0,
+            },
+            &Record {
+                time: 3,
+                x: 1,
+                y: 2,
+            },
+        );
+        assert_eq!(distance, 11);
+    }
     #[test]
     fn test_checkin1() {
         assert!(can_checkin(
@@ -34,6 +72,5 @@ mod tests {
                 y: 2,
             },
         ));
-        
     }
 }
