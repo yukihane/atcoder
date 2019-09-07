8af3ab739017a109f8c93003de2f1f51d59e406e
yukihane <yukihane.feather@gmail.com>
2019-09-07 17:09:12 +0900

    p3

--- atcoder139-java/src/main/java/Main.java
+++ atcoder139-java/src/main/java/Main.java
@@ -1,16 +1,115 @@
-import java.util.*;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(final String[] args) {
+        final int p = 4;
+        switch (p) {
+        case 4:
+            P4.answer();
+        case 3:
+            P3.answer();
+            break;
+        case 2:
+            P2.answer();
+            break;
+        case 1:
+            P1.p1();
+            break;
+        }
+    }
+}
+
+class P4 {
+    static void answer() {
         final Scanner sc = new Scanner(System.in);
-        // 整数の入力
+        final int n = sc.nextInt();
+        final int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        final int res = calc(array);
+        System.out.println(res);
+    }
+
+}
+
+class P3 {
+    static void answer() {
+        final Scanner sc = new Scanner(System.in);
+        final int n = sc.nextInt();
+        final int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        final int res = calc(array);
+        System.out.println(res);
+    }
+
+    static int calc(final int[] array) {
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (max > array.length - i) {
+                break;
+            }
+            final int res = measure(array, i);
+            max = Math.max(res, max);
+        }
+        return max;
+    }
+
+    private static int measure(final int[] array, final int pos) {
+        int times = 0;
+        for (int i = pos; i < array.length - 1; i++) {
+            if (array[i] >= array[i + 1]) {
+                times++;
+            } else {
+                break;
+            }
+        }
+        return times;
+    }
+
+}
+
+class P2 {
+    static void answer() {
+        final Scanner sc = new Scanner(System.in);
         final int a = sc.nextInt();
-        // スペース区切りの整数の入力
         final int b = sc.nextInt();
-        final int c = sc.nextInt();
-        // 文字列の入力
-        final String s = sc.next();
-        // 出力
-        System.out.println((a + b + c) + " " + s);
+        final int res = calc(a, b);
+        System.out.println(res);
+    }
+
+    static int calc(final int a, final int b) {
+        final int per = a - 1;
+        final int remain = b - 1;
+        final int x = remain / per;
+        if (remain - (per * x) > 0) {
+            return x + 1;
+        }
+        return x;
+    }
+}
+
+class P1 {
+
+    static void p1() {
+        final Scanner sc = new Scanner(System.in);
+        final String yohou = sc.nextLine();
+        final String jissai = sc.nextLine();
+
+        final int res = calc(yohou, jissai);
+        System.out.println(res);
+    }
+
+    static int calc(final String yohou, final String kekka) {
+        int res = 0;
+        for (int i = 0; i < 3; i++) {
+            if (Objects.equals(yohou.charAt(i), kekka.charAt(i))) {
+                res++;
+            }
+        }
+        return res;
     }
 }
--- atcoder139-java/src/test/java/MainTest.java
+++ atcoder139-java/src/test/java/MainTest.java
@@ -1,19 +1,28 @@
-
-
 import static org.junit.Assert.*;
 
-import org.junit.*;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class MainTest {
 
-    /**
-     * Rigourous Test :-)
-     */
     @Test
-    public void testApp() {
-        assertTrue(true);
+    public void test() {
+        assertEquals(2, P1.calc("CSS", "CSR"));
+        assertEquals(3, P1.calc("SSR", "SSR"));
+
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(3, P2.calc(4, 10));
+        assertEquals(2, P2.calc(8, 9));
+        assertEquals(1, P2.calc(8, 8));
+
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(2, P3.calc(new int[] { 10, 4, 8, 7, 3 }));
+        assertEquals(3, P3.calc(new int[] { 4, 4, 5, 6, 6, 5, 5 }));
+        assertEquals(0, P3.calc(new int[] { 1, 2, 3, 4 }));
     }
 }
