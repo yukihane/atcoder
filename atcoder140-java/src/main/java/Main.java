56a7f52d1639f49a604a845e715051a05a2ec00d
yukihane <yukihane.feather@gmail.com>
2019-09-07 23:25:25 +0900

    abc140

--- atcoder140-java/src/main/java/Main.java
+++ atcoder140-java/src/main/java/Main.java
@@ -1,13 +1,22 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(final String[] args) {
-        final int p = 1;
+        final int p = 5;
         switch (p) {
+        case 6:
+        case 5:
+            E.answer();
+            break;
+        case 4:
+            D.answer();
+            break;
+        case 3:
+            C.answer();
+            break;
+        case 2:
+            B.answer();
+            break;
         case 1:
             A.answer();
             break;
@@ -15,25 +24,166 @@ public class Main {
     }
 }
 
+class E {
+
+    static void answer() {
+        final Scanner sc = new Scanner(System.in);
+        final int n = sc.nextInt();
+        final int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        final int res = calc(array);
+        System.out.println(res);
+    }
+
+    static int calc(final int[] array) {
+        int total = 0;
+        final int prev[] = new int[] { -1, -1 };
+        for (int i = 0; i < array.length - 1; i++) {
+            final int largest[] = new int[] { array[i], -1 };
+            int subTotal = 0;
+            for (int j = i + 1; j < array.length; j++) {
+                final int num = array[j];
+                if (num != largest[0] && num != largest[1]) {
+                    if (num > largest[0]) {
+                        largest[1] = largest[0];
+                        largest[0] = num;
+                    } else if (num > largest[1]) {
+                        largest[1] = num;
+                    }
+                }
+//                System.out.println("" + i + "," + j + "," + num + "," + largest[0] + "," + largest[1]);
+                subTotal += largest[1];
+            }
+            total += subTotal;
+            prev[0] = largest[0];
+            prev[1] = largest[1];
+        }
+        return total;
+    }
+
+    private int second(final int[] subArray) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    private int[] makeSubArray(final int[] array, final int i, final int j) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
+
+class D {
+
+    static String drawUp(final String input, final int l, final int r) {
+        final char[] chars = input.toCharArray();
+        for (int i = l; i < r; i++) {
+            final char left = chars[i];
+            final char right = chars[r - i];
+            chars[i] = reverse(right);
+            chars[r - i] = reverse(left);
+        }
+        return String.copyValueOf(chars);
+    }
+
+    static char reverse(final char c) {
+        switch (c) {
+        case 'L':
+            return 'R';
+        case 'R':
+        default:
+            return 'L';
+        }
+    }
+
+    static void answer() {
+        final Scanner sc = new Scanner(System.in);
+        final int n = sc.nextInt();
+        final int[] array = new int[n];
+        array[0] = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+//
+//        final int res = calc(array);
+//        System.out.println(res);
+    }
+}
+
+class C {
+
+    static void answer() {
+        final Scanner sc = new Scanner(System.in);
+        final int n = sc.nextInt();
+        final int[] array = new int[n];
+        array[0] = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        final int res = calc(array);
+        System.out.println(res);
+    }
+
+    static int calc(final int[] array) {
+        int total = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            total += Math.min(array[i], array[i + 1]);
+        }
+        total += array[array.length - 1];
+        return total;
+    }
+}
+
+class B {
+
+    static void answer() {
+        final Scanner sc = new Scanner(System.in);
+        final int n = sc.nextInt();
+        final int[] arrayA = new int[n];
+        for (int i = 0; i < n; i++) {
+            arrayA[i] = sc.nextInt();
+        }
+        final int[] arrayB = new int[n];
+        for (int i = 0; i < n; i++) {
+            arrayB[i] = sc.nextInt();
+        }
+        final int[] arrayC = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            arrayC[i] = sc.nextInt();
+        }
+        final int res = calc(arrayA, arrayB, arrayC);
+        System.out.println(res);
+    }
+
+    static int calc(final int[] arrayA, final int[] arrayB, final int[] arrayC) {
+        int total = 0;
+        int prevRyori = -100;
+        for (int i = 0; i < arrayA.length; i++) {
+            final int ryoriNo = arrayA[i];
+            final int manpuku = arrayB[ryoriNo - 1];
+            total += manpuku;
+            if (ryoriNo - prevRyori == 1) {
+                total += arrayC[prevRyori - 1];
+            }
+            prevRyori = ryoriNo;
+        }
+
+        return total;
+    }
+}
+
 class A {
     static void answer() {
         final Scanner sc = new Scanner(System.in);
         final int n = sc.nextInt();
-        final List<LinkedList<Integer>> table = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
-            final LinkedList<Integer> ll = new LinkedList<>();
-            table.add(ll);
-            for (int j = 0; j < n - 1; j++) {
-                final int x = sc.nextInt();
-                ll.add(x);
-            }
-        }
-        final int res = calc(table);
+        final int res = calc(n);
         System.out.println(res);
     }
 
-    static int calc(final List<LinkedList<Integer>> table) {
-        // TODO Auto-generated method stub
-        return 0;
+    static int calc(final int n) {
+        return n * n * n;
     }
 }
--- atcoder140-java/src/test/java/MainTest.java
+++ atcoder140-java/src/test/java/MainTest.java
@@ -9,7 +9,29 @@ import org.junit.Test;
 public class MainTest {
 
     @Test
-    public void test() {
+    public void testE() {
+        assertEquals(5, E.calc(new int[] { 2, 3, 1 }));
+        assertEquals(30, E.calc(new int[] { 1, 2, 3, 4, 5 }));
+        assertEquals(136, E.calc(new int[] { 8, 2, 7, 3, 4, 5, 6, 1 }));
+    }
 
+    @Test
+    public void testC() {
+        assertEquals(9, C.calc(new int[] { 9999, 2, 5 }));
+        assertEquals(6, C.calc(new int[] { 9999, 3 }));
+        assertEquals(53, C.calc(new int[] { 9999, 0, 153, 10, 10, 23 }));
+    }
+
+    @Test
+    public void testB() {
+        assertEquals(14, B.calc(new int[] { 3, 1, 2 }, new int[] { 2, 5, 4 }, new int[] { 3, 6 }));
+        assertEquals(74, B.calc(new int[] { 2, 3, 4, 1 }, new int[] { 13, 5, 8, 24 }, new int[] { 45, 9, 15 }));
+        assertEquals(150, B.calc(new int[] { 1, 2 }, new int[] { 50, 50 }, new int[] { 50 }));
+    }
+
+    @Test
+    public void testA() {
+        assertEquals(8, A.calc(2));
+        assertEquals(1, A.calc(1));
     }
 }
