5cf3bf46080a2d80031e7183ccae4d560aef2f2a
yukihane <yukihane.feather@gmail.com>
2019-09-08 20:53:15 +0900

    138-b

--- atcoder/abc138/src/main.rs
+++ atcoder/abc138/src/main.rs
@@ -1,18 +1,71 @@
 fn main() {
-    let mode = "a";
+    let mode = "b";
 
     if mode == "a" {
         a::answer();
+    } else if mode == "b" {
+        b::answer();
     }
 }
-
-mod a {
-    use std::io::Read;
+mod b {
+    use std::io::*;
 
     pub fn answer() {
         let mut buf = String::new();
 
-        std::io::stdin().read_to_string(&mut buf).unwrap();
+        stdin().read_to_string(&mut buf).unwrap();
+        let mut ite = buf.split_whitespace();
+
+        // 1行目読み捨て
+        ite.next();
+
+        let seq = ite.map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>();
+
+        let res = calc(&seq);
+
+        println!("{}", res);
+    }
+    fn calc(a: &Vec<i32>) -> f64 {
+        let mut bunbo_neta = 1f64;
+        for i in 0..a.len() {
+            bunbo_neta *= a[i] as f64;
+        }
+        let mut bunbo = 0f64;
+        for i in 0..a.len() {
+            bunbo += bunbo_neta / (a[i] as f64);
+        }
+
+        let bunshi: f64 = bunbo_neta;
+
+        bunshi / bunbo
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn test1() {
+            assert_eq!(calc(&vec![10, 30]), 7.5);
+        }
+        #[test]
+        fn test2() {
+            let res = calc(&vec![200, 200, 200]);
+            assert!(res > 66.6666 && res < 66.67);
+        }
+        #[test]
+        fn test_b_3() {
+            let res = calc(&vec![1000]);
+            assert!(res == 1000.0);
+        }
+    }
+}
+mod a {
+    use std::io::*;
+
+    pub fn answer() {
+        let mut buf = String::new();
+
+        stdin().read_to_string(&mut buf).unwrap();
 
         let mut iter = buf.split_whitespace();
 
