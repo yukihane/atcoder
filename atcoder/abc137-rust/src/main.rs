27a7df040274a8b208fe927548af1709b6d92cff
yukihane <yukihane.feather@gmail.com>
2019-09-15 02:01:10 +0900

    abc137-c

--- atcoder/abc137-rust/src/main.rs
+++ atcoder/abc137-rust/src/main.rs
@@ -1,12 +1,114 @@
 fn main() {
-    let mode = "b";
+    let mode = "c";
     if mode == "a" {
         a::answer();
     } else if mode == "b" {
         b::answer();
+    } else if mode == "c" {
+        c::answer();
     }
 }
 
+mod c {
+    use std;
+    use std::io::Read;
+
+    pub fn answer() {
+        let mut buff = String::new();
+        std::io::stdin().read_to_string(&mut buff).unwrap();
+        let mut text = buff.split_whitespace();
+        text.next();
+        let input: Vec<&str> = text.into_iter().collect();
+
+        let res = calc(&input);
+        println!("{}", res);
+    }
+
+    fn calc(input: &Vec<&str>) -> u64 {
+        let sorted_str: Vec<String> = input.iter().map(|x| sort_string(x)).collect();
+        let sorted_text = sort_text(&sorted_str);
+        count(&sorted_text)
+    }
+
+    fn count(input: &Vec<String>) -> u64 {
+        let mut count = 0u64;
+        let mut stack = input.clone();
+
+        loop {
+            let elm = stack.pop();
+            match elm {
+                None => break,
+                Some(x) => {
+                    let sames = remove_sames(&x, &mut stack);
+                    count += sigma(sames);
+                }
+            }
+        }
+        count
+    }
+
+    fn sigma(number: u64) -> u64 {
+        (1..(number + 1)).fold(0, |a, b| a + b)
+    }
+
+    fn remove_sames(text: &str, stack: &mut Vec<String>) -> u64 {
+        let mut count = 0;
+
+        loop {
+            match stack.pop() {
+                None => break,
+                Some(x) => {
+                    if &x == text {
+                        count += 1;
+                    } else {
+                        stack.push(x);
+                        break;
+                    }
+                }
+            }
+        }
+
+        count
+    }
+
+    fn sort_text(input: &Vec<String>) -> Vec<String> {
+        let mut res = input.clone();
+        res.sort();
+        res
+    }
+
+    fn sort_string(input: &str) -> String {
+        let mut chars: Vec<_> = input.chars().collect();
+        chars.sort_by(|a, b| b.cmp(a));
+        chars.into_iter().collect()
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn test1() {
+            let input = vec!["acornistnt", "peanutbomb", "constraint"];
+            assert_eq!(calc(&input), 1u64);
+        }
+        #[test]
+        fn test2() {
+            let input = vec!["oneplustwo", "ninemodsix"];
+            assert_eq!(calc(&input), 0u64);
+        }
+        #[test]
+        fn test3() {
+            let input = vec![
+                "abaaaaaaaa",
+                "oneplustwo",
+                "aaaaaaaaba",
+                "twoplusone",
+                "aaaabaaaaa",
+            ];
+            assert_eq!(calc(&input), 4u64);
+        }
+    }
+}
 mod b {
     use std;
     use std::io::Read;
